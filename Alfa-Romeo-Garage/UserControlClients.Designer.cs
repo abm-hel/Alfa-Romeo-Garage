8c78bd10f6e2094c4e1658ee70c5d6cd9b1078b4
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-06-03 00:41:25 +0200

    Ajout des bouttons sur le user control - clients

--- Alfa-Romeo-Garage/UserControlClients.Designer.cs
+++ Alfa-Romeo-Garage/UserControlClients.Designer.cs
@@ -32,6 +32,11 @@ namespace Alfa_Romeo_Garage
             this.labelTitre = new System.Windows.Forms.Label();
             this.dataGridViewClients = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.buttonAjouterClient = new System.Windows.Forms.Button();
+            this.buttonEditerClient = new System.Windows.Forms.Button();
+            this.buttonSupprimerClient = new System.Windows.Forms.Button();
+            this.buttonConfirmerClient = new System.Windows.Forms.Button();
+            this.buttonAnnulerClient = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClients)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -50,11 +55,11 @@ namespace Alfa_Romeo_Garage
             // dataGridViewClients
             // 
             this.dataGridViewClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewClients.Location = new System.Drawing.Point(716, 131);
+            this.dataGridViewClients.Location = new System.Drawing.Point(595, 100);
             this.dataGridViewClients.Name = "dataGridViewClients";
             this.dataGridViewClients.RowHeadersWidth = 51;
             this.dataGridViewClients.RowTemplate.Height = 24;
-            this.dataGridViewClients.Size = new System.Drawing.Size(531, 659);
+            this.dataGridViewClients.Size = new System.Drawing.Size(613, 512);
             this.dataGridViewClients.TabIndex = 5;
             // 
             // dataGridView2
@@ -63,14 +68,95 @@ namespace Alfa_Romeo_Garage
             this.dataGridView2.Location = new System.Drawing.Point(789, 463);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
             this.dataGridView2.Size = new System.Drawing.Size(8, 8);
             this.dataGridView2.TabIndex = 6;
+            // 
+            // buttonAjouterClient
+            // 
+            this.buttonAjouterClient.BackColor = System.Drawing.Color.Crimson;
+            this.buttonAjouterClient.FlatAppearance.BorderSize = 0;
+            this.buttonAjouterClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAjouterClient.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAjouterClient.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonAjouterClient.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonAjouterClient.Location = new System.Drawing.Point(595, 631);
+            this.buttonAjouterClient.Name = "buttonAjouterClient";
+            this.buttonAjouterClient.Size = new System.Drawing.Size(613, 40);
+            this.buttonAjouterClient.TabIndex = 7;
+            this.buttonAjouterClient.Text = "Ajouter un client";
+            this.buttonAjouterClient.UseVisualStyleBackColor = false;
+            // 
+            // buttonEditerClient
+            // 
+            this.buttonEditerClient.BackColor = System.Drawing.Color.Crimson;
+            this.buttonEditerClient.FlatAppearance.BorderSize = 0;
+            this.buttonEditerClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEditerClient.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEditerClient.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonEditerClient.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonEditerClient.Location = new System.Drawing.Point(595, 686);
+            this.buttonEditerClient.Name = "buttonEditerClient";
+            this.buttonEditerClient.Size = new System.Drawing.Size(613, 40);
+            this.buttonEditerClient.TabIndex = 8;
+            this.buttonEditerClient.Text = "Editer un client";
+            this.buttonEditerClient.UseVisualStyleBackColor = false;
+            // 
+            // buttonSupprimerClient
+            // 
+            this.buttonSupprimerClient.BackColor = System.Drawing.Color.Crimson;
+            this.buttonSupprimerClient.FlatAppearance.BorderSize = 0;
+            this.buttonSupprimerClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSupprimerClient.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSupprimerClient.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSupprimerClient.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSupprimerClient.Location = new System.Drawing.Point(595, 741);
+            this.buttonSupprimerClient.Name = "buttonSupprimerClient";
+            this.buttonSupprimerClient.Size = new System.Drawing.Size(613, 40);
+            this.buttonSupprimerClient.TabIndex = 9;
+            this.buttonSupprimerClient.Text = "Supprimer un client";
+            this.buttonSupprimerClient.UseVisualStyleBackColor = false;
+            // 
+            // buttonConfirmerClient
+            // 
+            this.buttonConfirmerClient.BackColor = System.Drawing.Color.Crimson;
+            this.buttonConfirmerClient.FlatAppearance.BorderSize = 0;
+            this.buttonConfirmerClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConfirmerClient.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConfirmerClient.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonConfirmerClient.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonConfirmerClient.Location = new System.Drawing.Point(44, 741);
+            this.buttonConfirmerClient.Name = "buttonConfirmerClient";
+            this.buttonConfirmerClient.Size = new System.Drawing.Size(241, 40);
+            this.buttonConfirmerClient.TabIndex = 10;
+            this.buttonConfirmerClient.Text = "Confirmer";
+            this.buttonConfirmerClient.UseVisualStyleBackColor = false;
+            // 
+            // buttonAnnulerClient
+            // 
+            this.buttonAnnulerClient.BackColor = System.Drawing.Color.Crimson;
+            this.buttonAnnulerClient.FlatAppearance.BorderSize = 0;
+            this.buttonAnnulerClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAnnulerClient.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAnnulerClient.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonAnnulerClient.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonAnnulerClient.Location = new System.Drawing.Point(302, 741);
+            this.buttonAnnulerClient.Name = "buttonAnnulerClient";
+            this.buttonAnnulerClient.Size = new System.Drawing.Size(241, 40);
+            this.buttonAnnulerClient.TabIndex = 11;
+            this.buttonAnnulerClient.Text = "Annuler";
+            this.buttonAnnulerClient.UseVisualStyleBackColor = false;
             // 
             // UserControlClients
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.buttonAnnulerClient);
+            this.Controls.Add(this.buttonConfirmerClient);
+            this.Controls.Add(this.buttonSupprimerClient);
+            this.Controls.Add(this.buttonEditerClient);
+            this.Controls.Add(this.buttonAjouterClient);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridViewClients);
             this.Controls.Add(this.labelTitre);
@@ -88,5 +174,10 @@ namespace Alfa_Romeo_Garage
         private System.Windows.Forms.Label labelTitre;
         private System.Windows.Forms.DataGridView dataGridViewClients;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button buttonAjouterClient;
+        private System.Windows.Forms.Button buttonEditerClient;
+        private System.Windows.Forms.Button buttonSupprimerClient;
+        private System.Windows.Forms.Button buttonConfirmerClient;
+        private System.Windows.Forms.Button buttonAnnulerClient;
     }
 }
