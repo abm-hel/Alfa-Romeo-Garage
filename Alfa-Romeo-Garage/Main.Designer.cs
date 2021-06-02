6d4b77d323f4c43f0eae195a3f9ae636cb4283fb
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-06-02 06:08:58 +0200

    Rebuild le projet +Intégration du user control des clients dans la fenêtre principale

--- Alfa-Romeo-Garage/Main.Designer.cs
+++ Alfa-Romeo-Garage/Main.Designer.cs
@@ -31,10 +31,7 @@ namespace Alfa_Romeo_Garage
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.labelAccueil = new System.Windows.Forms.Label();
-            this.panelPosition = new System.Windows.Forms.Panel();
             this.panelButtonSelectionne = new System.Windows.Forms.Panel();
-            this.userControlAccueil1 = new Alfa_Romeo_Garage.UserControlAccueil();
             this.button5 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -42,7 +39,11 @@ namespace Alfa_Romeo_Garage
             this.pictureBoxLogoAccueil = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.buttonClients = new System.Windows.Forms.Button();
+            this.labelAccueil = new System.Windows.Forms.Label();
             this.buttonAccueil = new System.Windows.Forms.Button();
+            this.panelPosition = new System.Windows.Forms.Panel();
+            this.userControlAccueil1 = new Alfa_Romeo_Garage.UserControlAccueil();
+            this.userControlClients1 = new Alfa_Romeo_Garage.UserControlClients();
             this.panelMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoAccueil)).BeginInit();
             this.SuspendLayout();
@@ -67,28 +68,6 @@ namespace Alfa_Romeo_Garage
             this.panelMenu.Size = new System.Drawing.Size(312, 1002);
             this.panelMenu.TabIndex = 0;
             // 
-            // labelAccueil
-            // 
-            this.labelAccueil.AutoSize = true;
-            this.labelAccueil.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAccueil.ForeColor = System.Drawing.Color.Crimson;
-            this.labelAccueil.Location = new System.Drawing.Point(77, 87);
-            this.labelAccueil.Name = "labelAccueil";
-            this.labelAccueil.Size = new System.Drawing.Size(145, 46);
-            this.labelAccueil.TabIndex = 3;
-            this.labelAccueil.Text = "ALFA ROMEO \r\n    GARAGE";
-            // 
-            // panelPosition
-            // 
-            this.panelPosition.BackColor = System.Drawing.Color.Crimson;
-            this.panelPosition.Location = new System.Drawing.Point(313, 0);
-            this.panelPosition.Name = "panelPosition";
-            this.panelPosition.Size = new System.Drawing.Size(1260, 18);
-            this.panelPosition.TabIndex = 9;
-            this.panelPosition.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseDown);
-            this.panelPosition.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseMove);
-            this.panelPosition.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseUp);
-            // 
             // panelButtonSelectionne
             // 
             this.panelButtonSelectionne.BackColor = System.Drawing.Color.Crimson;
@@ -96,14 +75,6 @@ namespace Alfa_Romeo_Garage
             this.panelButtonSelectionne.Name = "panelButtonSelectionne";
             this.panelButtonSelectionne.Size = new System.Drawing.Size(14, 62);
             this.panelButtonSelectionne.TabIndex = 10;
-            // 
-            // userControlAccueil1
-            // 
-            this.userControlAccueil1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.userControlAccueil1.Location = new System.Drawing.Point(313, 197);
-            this.userControlAccueil1.Name = "userControlAccueil1";
-            this.userControlAccueil1.Size = new System.Drawing.Size(1260, 805);
-            this.userControlAccueil1.TabIndex = 10;
             // 
             // button5
             // 
@@ -205,6 +176,17 @@ namespace Alfa_Romeo_Garage
             this.buttonClients.Text = "Clients";
             this.buttonClients.UseVisualStyleBackColor = true;
             // 
+            // labelAccueil
+            // 
+            this.labelAccueil.AutoSize = true;
+            this.labelAccueil.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAccueil.ForeColor = System.Drawing.Color.Crimson;
+            this.labelAccueil.Location = new System.Drawing.Point(77, 87);
+            this.labelAccueil.Name = "labelAccueil";
+            this.labelAccueil.Size = new System.Drawing.Size(145, 46);
+            this.labelAccueil.TabIndex = 3;
+            this.labelAccueil.Text = "ALFA ROMEO \r\n    GARAGE";
+            // 
             // buttonAccueil
             // 
             this.buttonAccueil.FlatAppearance.BorderSize = 0;
@@ -220,12 +202,40 @@ namespace Alfa_Romeo_Garage
             this.buttonAccueil.Text = "Accueil";
             this.buttonAccueil.UseVisualStyleBackColor = true;
             // 
+            // panelPosition
+            // 
+            this.panelPosition.BackColor = System.Drawing.Color.Crimson;
+            this.panelPosition.Location = new System.Drawing.Point(313, 0);
+            this.panelPosition.Name = "panelPosition";
+            this.panelPosition.Size = new System.Drawing.Size(1260, 18);
+            this.panelPosition.TabIndex = 9;
+            this.panelPosition.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseDown);
+            this.panelPosition.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseMove);
+            this.panelPosition.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseUp);
+            // 
+            // userControlAccueil1
+            // 
+            this.userControlAccueil1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControlAccueil1.Location = new System.Drawing.Point(313, 202);
+            this.userControlAccueil1.Name = "userControlAccueil1";
+            this.userControlAccueil1.Size = new System.Drawing.Size(1260, 800);
+            this.userControlAccueil1.TabIndex = 10;
+            // 
+            // userControlClients1
+            // 
+            this.userControlClients1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControlClients1.Location = new System.Drawing.Point(313, 197);
+            this.userControlClients1.Name = "userControlClients1";
+            this.userControlClients1.Size = new System.Drawing.Size(1260, 805);
+            this.userControlClients1.TabIndex = 11;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1574, 1002);
+            this.Controls.Add(this.userControlClients1);
             this.Controls.Add(this.userControlAccueil1);
             this.Controls.Add(this.panelPosition);
             this.Controls.Add(this.panelMenu);
@@ -256,6 +266,7 @@ namespace Alfa_Romeo_Garage
         private System.Windows.Forms.Panel panelPosition;
         private System.Windows.Forms.Panel panelButtonSelectionne;
         private UserControlAccueil userControlAccueil1;
+        private UserControlClients userControlClients1;
     }
 }
 
