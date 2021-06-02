86fe6e6e9937ed2106bfd16dd6fa759525ae9ada
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-06-02 07:10:00 +0200

    Ajout du User Control pour les pièces

---
+++ Alfa-Romeo-Garage/UserControlPieces.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Alfa_Romeo_Garage
+{
+    partial class UserControlPieces
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTitre
+            // 
+            this.labelTitre.AutoSize = true;
+            this.labelTitre.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitre.ForeColor = System.Drawing.Color.Crimson;
+            this.labelTitre.Location = new System.Drawing.Point(39, 26);
+            this.labelTitre.Name = "labelTitre";
+            this.labelTitre.Size = new System.Drawing.Size(91, 30);
+            this.labelTitre.TabIndex = 5;
+            this.labelTitre.Text = "PIÈCES";
+            // 
+            // UserControlPieces
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.labelTitre);
+            this.Name = "UserControlPieces";
+            this.Size = new System.Drawing.Size(1260, 805);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitre;
+    }
+}
