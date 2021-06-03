43268c96b13fb816be4c3811c4f6c868d86da72f
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-06-03 02:30:18 +0200

    Complilation + tests

--- Alfa-Romeo-Garage/Main.Designer.cs
+++ Alfa-Romeo-Garage/Main.Designer.cs
@@ -32,8 +32,6 @@ namespace Alfa_Romeo_Garage
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.panelMenu = new System.Windows.Forms.Panel();
             this.panelButtonSelectionne = new System.Windows.Forms.Panel();
-            this.labelAccueil = new System.Windows.Forms.Label();
-            this.panelPosition = new System.Windows.Forms.Panel();
             this.buttonInterventions = new System.Windows.Forms.Button();
             this.buttonEntretiens = new System.Windows.Forms.Button();
             this.buttonPieces = new System.Windows.Forms.Button();
@@ -41,14 +39,16 @@ namespace Alfa_Romeo_Garage
             this.pictureBoxLogoAccueil = new System.Windows.Forms.PictureBox();
             this.buttonModeles = new System.Windows.Forms.Button();
             this.buttonClients = new System.Windows.Forms.Button();
+            this.labelAccueil = new System.Windows.Forms.Label();
             this.buttonAccueil = new System.Windows.Forms.Button();
+            this.panelPosition = new System.Windows.Forms.Panel();
+            this.userControlEntretiens1 = new Alfa_Romeo_Garage.UserControlEntretiens();
+            this.userControlIntervetions1 = new Alfa_Romeo_Garage.UserControlIntervetions();
+            this.userControlPieces1 = new Alfa_Romeo_Garage.UserControlPieces();
+            this.userControlVehiculescs1 = new Alfa_Romeo_Garage.UserControlVehicules();
+            this.userControlModeles1 = new Alfa_Romeo_Garage.UserControlModeles();
             this.userControlClients1 = new Alfa_Romeo_Garage.UserControlClients();
             this.userControlAccueil1 = new Alfa_Romeo_Garage.UserControlAccueil();
-            this.userControlModeles1 = new Alfa_Romeo_Garage.UserControlModeles();
-            this.userControlVehiculescs1 = new Alfa_Romeo_Garage.UserControlVehicules();
-            this.userControlPieces1 = new Alfa_Romeo_Garage.UserControlPieces();
-            this.userControlIntervetions1 = new Alfa_Romeo_Garage.UserControlIntervetions();
-            this.userControlEntretiens1 = new Alfa_Romeo_Garage.UserControlEntretiens();
             this.panelMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogoAccueil)).BeginInit();
             this.SuspendLayout();
@@ -80,28 +80,6 @@ namespace Alfa_Romeo_Garage
             this.panelButtonSelectionne.Name = "panelButtonSelectionne";
             this.panelButtonSelectionne.Size = new System.Drawing.Size(14, 62);
             this.panelButtonSelectionne.TabIndex = 10;
-            // 
-            // labelAccueil
-            // 
-            this.labelAccueil.AutoSize = true;
-            this.labelAccueil.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAccueil.ForeColor = System.Drawing.Color.Crimson;
-            this.labelAccueil.Location = new System.Drawing.Point(77, 87);
-            this.labelAccueil.Name = "labelAccueil";
-            this.labelAccueil.Size = new System.Drawing.Size(145, 46);
-            this.labelAccueil.TabIndex = 3;
-            this.labelAccueil.Text = "ALFA ROMEO \r\n    GARAGE";
-            // 
-            // panelPosition
-            // 
-            this.panelPosition.BackColor = System.Drawing.Color.Crimson;
-            this.panelPosition.Location = new System.Drawing.Point(313, 0);
-            this.panelPosition.Name = "panelPosition";
-            this.panelPosition.Size = new System.Drawing.Size(1260, 18);
-            this.panelPosition.TabIndex = 9;
-            this.panelPosition.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseDown);
-            this.panelPosition.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseMove);
-            this.panelPosition.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseUp);
             // 
             // buttonInterventions
             // 
@@ -209,6 +187,17 @@ namespace Alfa_Romeo_Garage
             this.buttonClients.UseVisualStyleBackColor = true;
             this.buttonClients.Click += new System.EventHandler(this.buttonClients_Click);
             // 
+            // labelAccueil
+            // 
+            this.labelAccueil.AutoSize = true;
+            this.labelAccueil.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAccueil.ForeColor = System.Drawing.Color.Crimson;
+            this.labelAccueil.Location = new System.Drawing.Point(77, 87);
+            this.labelAccueil.Name = "labelAccueil";
+            this.labelAccueil.Size = new System.Drawing.Size(145, 46);
+            this.labelAccueil.TabIndex = 3;
+            this.labelAccueil.Text = "ALFA ROMEO \r\n    GARAGE";
+            // 
             // buttonAccueil
             // 
             this.buttonAccueil.FlatAppearance.BorderSize = 0;
@@ -225,6 +214,57 @@ namespace Alfa_Romeo_Garage
             this.buttonAccueil.UseVisualStyleBackColor = true;
             this.buttonAccueil.Click += new System.EventHandler(this.buttonAccueil_Click);
             // 
+            // panelPosition
+            // 
+            this.panelPosition.BackColor = System.Drawing.Color.Crimson;
+            this.panelPosition.Location = new System.Drawing.Point(313, 0);
+            this.panelPosition.Name = "panelPosition";
+            this.panelPosition.Size = new System.Drawing.Size(1260, 18);
+            this.panelPosition.TabIndex = 9;
+            this.panelPosition.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseDown);
+            this.panelPosition.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseMove);
+            this.panelPosition.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelPosition_MouseUp);
+            // 
+            // userControlEntretiens1
+            // 
+            this.userControlEntretiens1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControlEntretiens1.Location = new System.Drawing.Point(313, 197);
+            this.userControlEntretiens1.Name = "userControlEntretiens1";
+            this.userControlEntretiens1.Size = new System.Drawing.Size(1260, 805);
+            this.userControlEntretiens1.TabIndex = 16;
+            // 
+            // userControlIntervetions1
+            // 
+            this.userControlIntervetions1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControlIntervetions1.Location = new System.Drawing.Point(313, 197);
+            this.userControlIntervetions1.Name = "userControlIntervetions1";
+            this.userControlIntervetions1.Size = new System.Drawing.Size(1260, 805);
+            this.userControlIntervetions1.TabIndex = 15;
+            // 
+            // userControlPieces1
+            // 
+            this.userControlPieces1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControlPieces1.Location = new System.Drawing.Point(313, 197);
+            this.userControlPieces1.Name = "userControlPieces1";
+            this.userControlPieces1.Size = new System.Drawing.Size(1260, 805);
+            this.userControlPieces1.TabIndex = 14;
+            // 
+            // userControlVehiculescs1
+            // 
+            this.userControlVehiculescs1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControlVehiculescs1.Location = new System.Drawing.Point(313, 197);
+            this.userControlVehiculescs1.Name = "userControlVehiculescs1";
+            this.userControlVehiculescs1.Size = new System.Drawing.Size(1260, 805);
+            this.userControlVehiculescs1.TabIndex = 13;
+            // 
+            // userControlModeles1
+            // 
+            this.userControlModeles1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.userControlModeles1.Location = new System.Drawing.Point(313, 193);
+            this.userControlModeles1.Name = "userControlModeles1";
+            this.userControlModeles1.Size = new System.Drawing.Size(1260, 805);
+            this.userControlModeles1.TabIndex = 12;
+            // 
             // userControlClients1
             // 
             this.userControlClients1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
@@ -240,46 +280,6 @@ namespace Alfa_Romeo_Garage
             this.userControlAccueil1.Name = "userControlAccueil1";
             this.userControlAccueil1.Size = new System.Drawing.Size(1260, 800);
             this.userControlAccueil1.TabIndex = 10;
-            // 
-            // userControlModeles1
-            // 
-            this.userControlModeles1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.userControlModeles1.Location = new System.Drawing.Point(313, 193);
-            this.userControlModeles1.Name = "userControlModeles1";
-            this.userControlModeles1.Size = new System.Drawing.Size(1260, 805);
-            this.userControlModeles1.TabIndex = 12;
-            // 
-            // userControlVehiculescs1
-            // 
-            this.userControlVehiculescs1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.userControlVehiculescs1.Location = new System.Drawing.Point(313, 197);
-            this.userControlVehiculescs1.Name = "userControlVehiculescs1";
-            this.userControlVehiculescs1.Size = new System.Drawing.Size(1260, 805);
-            this.userControlVehiculescs1.TabIndex = 13;
-            // 
-            // userControlPieces1
-            // 
-            this.userControlPieces1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.userControlPieces1.Location = new System.Drawing.Point(313, 197);
-            this.userControlPieces1.Name = "userControlPieces1";
-            this.userControlPieces1.Size = new System.Drawing.Size(1260, 805);
-            this.userControlPieces1.TabIndex = 14;
-            // 
-            // userControlIntervetions1
-            // 
-            this.userControlIntervetions1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.userControlIntervetions1.Location = new System.Drawing.Point(313, 197);
-            this.userControlIntervetions1.Name = "userControlIntervetions1";
-            this.userControlIntervetions1.Size = new System.Drawing.Size(1260, 805);
-            this.userControlIntervetions1.TabIndex = 15;
-            // 
-            // userControlEntretiens1
-            // 
-            this.userControlEntretiens1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.userControlEntretiens1.Location = new System.Drawing.Point(313, 197);
-            this.userControlEntretiens1.Name = "userControlEntretiens1";
-            this.userControlEntretiens1.Size = new System.Drawing.Size(1260, 805);
-            this.userControlEntretiens1.TabIndex = 16;
             // 
             // MainForm
             // 
